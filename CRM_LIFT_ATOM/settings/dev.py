59b267b4f5d36f51f6728476160a45465c7f8b4b
pavi <rkpavi06@gmail.com>
2025-10-29 10:06:12 +0530

    chore: update Gmail SMTP credentials in dev settings
    Updated EMAIL_HOST_USER from "clh.test.vlt@gmail.com" to "atom.lift.1@gmail.com" and corresponding EMAIL_HOST_PASSWORD in CRM_LIFT_ATOM/settings/dev.py. This reflects a switch to a dedicated Gmail account for handling outgoing emails in the development environment, ensuring proper testing and email functionality.

--- CRM_LIFT_ATOM/settings/dev.py
+++ CRM_LIFT_ATOM/settings/dev.py
@@ -14,8 +14,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "clh.test.vlt@gmail.com"
-EMAIL_HOST_PASSWORD = "uoqojfyrdcchathu"
+EMAIL_HOST_USER = "atom.lift.1@gmail.com"
+EMAIL_HOST_PASSWORD = "rldeunwqzvkmltnv"
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.technuob.com",
