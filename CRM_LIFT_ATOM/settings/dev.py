2f08c024370896204356684c1e0d6cd715ee9a97
Suriya D'cruze <suryanantakumar0007@gmail.com>
2025-12-02 09:26:05 +0530

    Add additional allowed hosts for development

--- CRM_LIFT_ATOM/settings/dev.py
+++ CRM_LIFT_ATOM/settings/dev.py
@@ -24,6 +24,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://localhost:8081",
+    "https://admin.careelevators.in/",
+    "https://technician.careelevators.in/",
 ]
 
 # Note: CSRF middleware is enabled for Wagtail admin security
